997f1504aacb571ae7fa5ebc10d518a1ee0308ea
Jason Candelora <jasonatsmtc@hotmail.com>
2017-11-18 14:59:02 -0500

    initial commit

---
+++ Services/ItemController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Security;
+using System.Web.Http;
+using TestData;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using TestData.Models;
+using TestData.Utils;
+
+namespace AAModules.EnhancedContactFormEnhancedContactForm.Services
+{
+    [DnnAuthorize]
+    public class ItemController : DnnApiController
+    {
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            using(var db = DbUtil.GetConnection())
+            {
+                return Item.GetAll(db);
+            }
+        }
+
+        [HttpGet]
+        public Item GetItem(int itemId)
+        {
+            using (var db = DbUtil.GetConnection())
+            {
+                return Item.GetById(db, itemId);
+            }
+        }
+    }
+}
